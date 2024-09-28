3687600a8509db7aa96a5df26b9c88f06f4bb77c
kibaidar1 <aidar1996@gmail.com>
2024-09-28 23:53:32 +0300

    Changed name role table

--- src/auth/models.py
+++ src/auth/models.py
@@ -2,13 +2,13 @@ from datetime import datetime
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import Integer, String, TIMESTAMP, JSON, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database import Base
 
 
 class Role(Base):
-    __tablename__ = "roles"
+    __tablename__ = "role"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, unique=True)
     permission: Mapped[JSON] = mapped_column(JSON, nullable=True)
@@ -17,5 +17,5 @@ class Role(Base):
 class User(SQLAlchemyBaseUserTable[int], Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String)
-    role: Mapped[ForeignKey] = mapped_column(ForeignKey(Role.id))
+    role: Mapped[ForeignKey] = mapped_column(ForeignKey(Role.id, ondelete='CASCADE'))
     registered_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, default=datetime.now())
