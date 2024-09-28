df1b451f34fd1272d7ceeadfb6110e4c2634a7ae
kibaidar1 <aidar1996@gmail.com>
2024-09-28 23:55:09 +0300

    Added authorization requirement for assistants

--- src/assistant/router.py
+++ src/assistant/router.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.assistant.models import Assistant
 from src.assistant.schemas import AssistantRead, AssistantCreate
+from src.auth.base_config import current_active_user
+from src.auth.models import User
 from src.database import get_async_session
 
 router = APIRouter(
@@ -15,21 +17,24 @@ router = APIRouter(
 
 
 @router.get('/', response_model=List[AssistantRead])
-async def get_assistants(session: AsyncSession = Depends(get_async_session)):
+async def get_assistants(session: AsyncSession = Depends(get_async_session),
+                         user: User = Depends(current_active_user)):
     query = select(Assistant)
     assistants = await session.execute(query)
     return assistants.all()
 
 
 @router.get('/{assistant_id}', response_model=AssistantRead)
-async def get_assistant(assistant_id: int, session: AsyncSession = Depends(get_async_session)):
+async def get_assistant(assistant_id: int, session: AsyncSession = Depends(get_async_session),
+                        user: User = Depends(current_active_user)):
     query = select(Assistant).where(Assistant.id == assistant_id)
     result = await session.execute(query)
     return result.scalar_one()
 
 
 @router.post('/', response_model=AssistantCreate)
-async def add_assistant(assistant: AssistantCreate, session: AsyncSession = Depends(get_async_session)):
+async def add_assistant(assistant: AssistantCreate, session: AsyncSession = Depends(get_async_session),
+                        user: User = Depends(current_active_user)):
     stmt = insert(Assistant).values(**assistant.model_dump())
     await session.execute(stmt)
     await session.commit()
