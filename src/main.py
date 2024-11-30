80466e978af974aaf2484abea86e51be79a3f157
kibaidar1 <aidar1996@gmail.com>
2024-11-30 15:42:27 +0300

    Fixet fail

--- src/main.py
+++ src/main.py
@@ -40,7 +40,7 @@ admin.add_view(ProfileAdmin)
 origins = ['http://localhost:5174', 'http://http://127.0.0.1:5174']
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
